3f2bbf717d3c79d1e911c5e75a5a51bcb97423e5
funkyplants <kaso20fg@student.ju.se>
2023-06-09 14:57:25 +0200

    config updates

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,16 @@
-import { fileURLToPath, URL } from "node:url";
-
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      "@": "/src",
     },
   },
   build: {
     rollupOptions: {
-      external: [
-        /\.css$/,
-        /\.(png|jpg)$/, // Add this line to include both PNG and JPG files
-      ],
+      external: [/\.css$/, /\.(png|jpe?g|gif|svg|webp)$/i],
     },
   },
 });
